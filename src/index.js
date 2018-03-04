d91c672031b5dde4ed1654b90f608ad42fdc4a7f
Daria Cheryomuhina <daria.cheryuomuhina@gmail.com>
2018-03-05 00:40:15 +0300

    Corrects router basename.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App/App';
 
 ReactDOM.render(
-  <Router>
+  <Router basename="/test-unsplash">
     <App />
   </Router>,
   document.getElementById('root')
